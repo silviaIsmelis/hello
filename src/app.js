cf4d7fa98a665e31523e4a8d99502925dd752edc
silviaismelish <silviaismelish@gmail.com>
2022-10-30 20:49:10 -0700

    update2

--- src/app.js
+++ src/app.js
@@ -4,23 +4,21 @@ const cors = require('cors');
 const app = express();
 
 // Importar conexion a mongoDB
-const database=require('./database') 
+const database = require('./database') 
 
-// settings
+// configuracion del puerto
 app.set('port', process.env.PORT || 4000); 
 
 // middlewares
 app.use(cors());
 app.use(express.json());
 
-console.log("Hola1");
-
 // importacion de routes y modelos
-const rutaUser=require('./routes/user')
-const rutaNota=require('./routes/notes')
+const rutaUser = require('./routes/user')
+const rutaNota = require('./routes/notes')
 
 // importar body-parser para convertir la peticion en JSON
-const bodyParser= require('body-parser')
+const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:'true'}))
 
